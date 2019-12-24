972b2f820837f34f10aa8ac63e98975186c16ab3
jkwu <jkwu@amarsoft.com>
2019-12-25 00:06:32 +0800

    fix: fix isNewDay function add count query

--- cache.js
+++ cache.js
@@ -1,9 +1,15 @@
-const isNewDay = (currentDate) => {
-return currentDate.getHours() === 0 && currentDate.getMinutes() === 0 && currentDate.getSeconds() === 0;
+const isNewDay = (cacheDate) => {
+  const currentDate = new Date();
+  if (currentDate.getFullYear() !== new Date(cacheDate).getFullYear()) return true;
+  if (currentDate.getMonth() !== new Date(cacheDate).getMonth()) return true;
+  if (currentDate.getDate() !== new Date(cacheDate).getDate()) return true;
+  if (currentDate.getDay() !== new Date(cacheDate).getDay()) return true;
+  return currentDate.getHours() === 0 && currentDate.getMinutes() === 0 && currentDate.getSeconds() === 0;
 };
 class CacheHandle {
   constructor() {
     this.caches = {};
+    this.count = 0;
   }
 
   setCacheItem (api, item) {
@@ -11,13 +17,20 @@ class CacheHandle {
   }
 
   getCacheItem (api) {
-    const currentDate = new Date();
-    if (isNewDay(currentDate)) this.clearCache();
+    if (isNewDay(this.caches[api] && this.caches[api].time || new Date())) this.clearCache();
     return this.caches[api];
   }
   clearCache() {
     console.log(`---------清空缓存时间${new Date().toLocaleString()}-------`);
     this.caches = {};
+  }
+
+  addCount() {
+   this.count += 1;
+  }
+
+  getCount() {
+    return this.count;
   }
 };
 
@@ -32,8 +45,9 @@ const Cache = () => {
         console.log(`${ctx.url}:缓存数据:缓存时间=${cacheDataSource.time}:内容=${content}`);
         ctx.body = content;
       } else {
+        cacheHandle.addCount();
         const content = `${Math.random() * 1000000}`;
-        console.log(`${ctx.url}:查询数据:查询时间=${new Date().toLocaleString()}:内容=${content}`);
+        console.log(`${ctx.url}:查询数据:查询时间=${new Date().toLocaleString()}:内容=${content}:查询总次数为：${cacheHandle.getCount()}`);
         ctx.body = content;
         cacheHandle.setCacheItem(ctx.url, content);
       }
